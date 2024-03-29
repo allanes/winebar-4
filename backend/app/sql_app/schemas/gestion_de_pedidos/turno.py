735416471cb8c0a9b4908c2c8847a88e6a2562e3
Adrian Llanes <ados.adrian@gmail.com>
2024-03-28 21:51:34 -0300

    agregados campos de abierto y cerrado_por_nombre con serializadores de salida para schema turno

--- backend/app/sql_app/schemas/gestion_de_pedidos/turno.py
+++ backend/app/sql_app/schemas/gestion_de_pedidos/turno.py
@@ -1,6 +1,7 @@
-from pydantic import BaseModel, ConfigDict
+from pydantic import BaseModel, ConfigDict, field_serializer
 from datetime import datetime
 from typing import Optional
+from ..serializers import serializer_for_nombre_personal
 
 class TurnoBase(BaseModel):
     abierto_por: int
@@ -28,6 +29,18 @@ class TurnoInDBBase(TurnoBase):
 class Turno(TurnoInDBBase):
     clientes_activos: Optional[int] = 0
     suma_ordenes_cobradas: Optional[float] = 0
+    abierto_por_nombre: Optional[str] = ''
+    cerrado_por_nombre: Optional[str] = ''
+
+    @field_serializer('abierto_por_nombre')
+    def serialize_nombre_abierto(self, abierto_por_nombre: datetime, _info):
+        nombre_completo = serializer_for_nombre_personal(self.abierto_por)
+        return  nombre_completo
+    
+    @field_serializer('cerrado_por_nombre')
+    def serialize_nombre_cerrado(self, cerrado_por_nombre: datetime, _info):
+        nombre_completo = serializer_for_nombre_personal(self.cerrado_por)
+        return  nombre_completo
 
 class TurnoInDB(TurnoInDBBase):
     pass
