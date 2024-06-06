85b82cbfcbe4c818f2769b3feb1c7047e9a09d82
Adrian Llanes <ados.adrian@gmail.com>
2024-06-06 18:42:19 -0300

    fix de un print

--- backend/app/sql_app/lifespan_handlers.py
+++ backend/app/sql_app/lifespan_handlers.py
@@ -50,7 +50,7 @@ def should_initialize_db(db: Session) -> bool:
     print('Chequeando db...')
     should = crud.personal_interno.get_multi(db, only_active=False)
     if not should:
-        'Necesita inicializar. Mandando Señal...'
+        print('Necesita inicializar. Mandando Señal...')
         return True
     print('Salteando inicializacion de db. La base de datos ya se encuentra inicializada.')
     return False
